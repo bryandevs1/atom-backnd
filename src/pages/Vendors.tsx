6c593b09b3e8684dabedcb52c86292c14e1ab874
Bryan Ojji <bryanojji4@gmail.com>
2025-06-22 00:03:32 +0100

    lolo

--- src/pages/Vendors.tsx
+++ src/pages/Vendors.tsx
@@ -11,16 +11,14 @@ const Calendar: React.FC = () => {
   return (
     <>
       <PageMeta
-        title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="React.js Basic Tables Dashboard |  "
+        description="This is React.js Basic Tables Dashboard page for  Atommarts"
       />
       <PageBreadcrumb pageTitle="All Orders" />
       <div className="space-y-6">
-
         <ComponentCard title="Orders">
           <BasicTableFour />
         </ComponentCard>
-
       </div>
     </>
   );
